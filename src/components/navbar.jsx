cc694c4fe4b78dbe9ae76083a785c32049ba6d81
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-07-11 12:00:07 -0300

    add increment, delete and reset buttons. add navBar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
